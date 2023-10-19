153bc0fb1352842d8c47570b003e081624026306
tangzicheng <43260130+tangzichengcc@users.noreply.github.com>
2023-10-19 13:27:02 +0000

    update

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -15,7 +14,11 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            let firstc = first.to_uppercase().collect::<String>();
+            let rest = c.collect::<String>();
+            firstc + &rest
+        },
     }
 }
 
@@ -24,7 +27,11 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut capitalize_words = Vec::new();
+    for word in words{
+        capitalize_words.push(capitalize_first(word));
+    }
+    capitalize_words
 }
 
 // Step 3.
@@ -32,7 +39,11 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut str1 = String::new();
+    for word in words{
+        str1 = str1 + &capitalize_first(word);
+    }
+    str1
 }
 
 #[cfg(test)]
--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,10 +3,8 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-use std::sync::mpsc;
-use std::sync::Arc;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -25,42 +23,49 @@ impl Queue {
         }
     }
 }
+fn send_tx(q: Arc<Mutex<Queue>>, tx: Arc<Mutex<mpsc::Sender<u32>>>) {
+    let qc = Arc::clone(&q);
+    let qc1 = Arc::clone(&q);
 
-fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
-    let qc = Arc::new(q);
-    let qc1 = Arc::clone(&qc);
-    let qc2 = Arc::clone(&qc);
+    let tx1 = Arc::clone(&tx);  // Clone tx for the first thread
+    let tx2 = Arc::clone(&tx);  // Clone tx for the second thread
 
     thread::spawn(move || {
-        for val in &qc1.first_half {
+        for val in &qc.lock().unwrap().first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.lock().unwrap().send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
     thread::spawn(move || {
-        for val in &qc2.second_half {
+        for val in &qc1.lock().unwrap().second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.lock().unwrap().send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 }
 
+
+
 fn main() {
     let (tx, rx) = mpsc::channel();
-    let queue = Queue::new();
-    let queue_length = queue.length;
+    let queue = Arc::new(Mutex::new(Queue::new()));
+    let queue_length = queue.lock().unwrap().length;
+    let tx = Arc::new(Mutex::new(tx));  // Wrap the Sender in an Arc<Mutex>
 
-    send_tx(queue, tx);
+    send_tx(Arc::clone(&queue), Arc::clone(&tx));  // Pass the wrapped Sender
 
     let mut total_received: u32 = 0;
     for received in rx {
         println!("Got: {}", received);
         total_received += 1;
+        if total_received == queue_length {
+            break;
+        }
     }
 
     println!("total numbers received: {}", total_received);
-    assert_eq!(total_received, queue_length)
+    assert_eq!(total_received, queue_length);
 }
